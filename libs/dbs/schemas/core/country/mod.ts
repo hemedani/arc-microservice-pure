c9409fd8af92cc3bf368a21dd18f8ca7a62e97a3
zsattari <famil.shia14@gmail.com>
2022-04-04 18:03:39 +0430

    add mod to schemas

--- libs/dbs/schemas/core/country/mod.ts
+++ libs/dbs/schemas/core/country/mod.ts
@@ -1,7 +1,7 @@
 import { assign, Infer } from "https://deno.land/x/lestruct@v0.0.1/mod.ts";
 import db from "../../../db.ts";
-import { userEmbedded } from "./embed.ts";
-import { pureUser } from "./pure.ts";
+import { countryEmbedded } from "./embed.ts";
+import { pureCountry } from "./pure.ts";
 
 // export interface RUser {
 //   _id: RType;
@@ -23,11 +23,11 @@ import { pureUser } from "./pure.ts";
 //   isActive?: RType;
 // }
 
-export const userObj = assign(userEmbedded, pureUser);
+export const countryObj = assign(countryEmbedded, pureCountry);
 
-export type User = Infer<typeof userObj>;
+export type Country = Infer<typeof countryObj>;
 
-export const user = db.collection<User>("User");
+export const country = db.collection<Country>("Country");
 
 export * from "./embed.ts";
 export * from "./inRel.ts";
